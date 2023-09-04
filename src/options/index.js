0e4b3bc6fc350946178dc431760db59f728d6586
lovezsh <shuxhan@163.com>
2023-09-04 17:54:11 +0800

    2.0版本发布，重构了显示设置

--- src/options/index.js
+++ src/options/index.js
@@ -1,6 +1,17 @@
-import 'bulma-fluent/bulma.sass'
-import Vue from 'vue'
-import App from './App.vue'
+import 'bulma-fluent/bulma.sass';
+import Vue from 'vue';
+import App from './App.vue';
+
+import VueSimpleMarkdown from 'vue-simple-markdown';
+import 'vue-simple-markdown/dist/vue-simple-markdown.css';
+
+Vue.use(VueSimpleMarkdown)
+import Vuesax from 'vuesax';
+import 'vuesax/dist/vuesax.css';
+
+Vue.use(Vuesax, {
+  // options here
+})
 
 // eslint-disable-next-line
 new Vue({
--- src/popup/index.js
+++ src/popup/index.js
@@ -2,17 +2,6 @@ import 'bulma-fluent/bulma.sass';
 import Vue from 'vue';
 import App from './App.vue';
 
-import VueSimpleMarkdown from 'vue-simple-markdown';
-import 'vue-simple-markdown/dist/vue-simple-markdown.css';
-
-Vue.use(VueSimpleMarkdown)
-import Vuesax from 'vuesax';
-import 'vuesax/dist/vuesax.css';
-
-Vue.use(Vuesax, {
-  // options here
-})
-
 // eslint-disable-next-line
 new Vue({
   el: '#app',
